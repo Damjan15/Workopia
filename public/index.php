af39bb12c3c504008c5c32c6533ef7801f9e184c
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-13 11:21:08 +0100

    feat: added database configurations

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../helpers.php';
 
+require basePath('Database.php');
 require basePath('Router.php');
 
 $router = new Router();
@@ -10,3 +11,6 @@ $uri = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
 
 $router->route($uri, $method);
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
