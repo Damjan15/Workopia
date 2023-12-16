4c2642e292d924e13b4c6d0776c5d4f38b033ae1
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-16 13:23:15 +0100

    feat: UseController setup, login and register page setup

--- routes.php
+++ routes.php
@@ -8,3 +8,5 @@ $router->post('/listings', 'ListingController@store');
 $router->delete('/listings/delete/{id}', 'ListingController@destroy');
 $router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->put('/listings/{id}', 'ListingController@update');
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
