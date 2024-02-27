ef5071e20d1469e7d10bd1ec92cdcf6c7d8a357a
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-27 17:01:34 +0100

    refactor: router changed to class, added methods

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+require basePath('Router.php');
 
-require basePath('router.php');
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router = new Router;
+$routes = require basePath('routes.php');
+$router->route($uri, $method);
