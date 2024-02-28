398969f510f92e331348085822a8f335df29990d
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-28 21:33:45 +0100

    feat: created class for connecting to the Database

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require '../helpers.php';
 
 require basePath('Router.php');
+require basePath('Database.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
@@ -10,3 +11,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 $router = new Router;
 $routes = require basePath('routes.php');
 $router->route($uri, $method);
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
