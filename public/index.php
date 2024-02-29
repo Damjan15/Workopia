52622f2a3ab3abe88e7ac5d892847f69aa501aa9
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-29 12:03:08 +0100

    refactor: folder structure

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 require '../helpers.php';
 
-require basePath('Router.php');
-require basePath('Database.php');
+require basePath('Framework/Router.php');
+require basePath('Framework/Database.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
