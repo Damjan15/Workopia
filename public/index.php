c66d5fe42b8deb2fe4b32e252c48f3f7b3f1b2f1
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-29 12:18:43 +0100

    feat: ps4-autoloader created, namespaces added

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
-require '../helpers.php';
+use Framework\Router;
+use Framework\Database;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../helpers.php';
 
 require basePath('Framework/Router.php');
 require basePath('Framework/Database.php');
