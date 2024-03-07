e330d2094638a5f7ddf222ad2dee15bfea713de0
Damjan15 <stamenovdamjan5@gmail.com>
2024-03-07 09:02:46 +0100

    feat: search functionality created

--- routes.php
+++ routes.php
@@ -4,6 +4,7 @@ $router->get('/', 'HomeController@index');
 
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create', ['auth']);
+$router->get('/listings/search', 'ListingController@search');
 $router->get('/listing/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store', ['auth']);
 $router->delete('/listings/delete/{id}', 'ListingController@destroy', ['auth']);
