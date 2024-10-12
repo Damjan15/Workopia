a58d67bf999bbfdef2f0f7eb656c9dc1a27fc462
Damjan15 <stamenovdamjan5@gmail.com>
2024-10-13 00:44:22 +0200

    feat: helper functions for loading views and loadingpartials created, refactored folder structure, publi folder created

--- helpers.php
+++ helpers.php
@@ -10,3 +10,28 @@ function basePath($path = "")
 {
     return __DIR__ . '/' . $path;
 }
+
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadView($name)
+{
+    require basePath("views/{$name}.view.php");
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadPartial($name)
+{
+    require basePath("views/partials/{$name}.php");
+}
