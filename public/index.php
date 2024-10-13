3b7d42ce088cf5a8ef392f4c37cd2fe22948a5f1
Damjan15 <stamenovdamjan5@gmail.com>
2024-10-13 10:47:58 +0200

    feat: router refactored to class

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+require basePath('Router.php');
+$router = new Router;
 
-require basePath('router.php');
+$routes = require basePath('routes.php');
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
