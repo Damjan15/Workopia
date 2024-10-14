691aca3625401cfe40f68081d2676c1f9c3f0a94
Damjan15 <stamenovdamjan5@gmail.com>
2024-10-14 21:47:47 +0200

    feat: query method added to our Database class

--- controllers/home.php
+++ controllers/home.php
@@ -1,3 +1,11 @@
 <?php
 
+require basePath('Database.php');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+inspect($listings);
+
 loadView('home');
