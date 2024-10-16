2bab993611ce8491927d6fdfbe2912976c178d51
Damjan15 <stamenovdamjan5@gmail.com>
2024-10-16 14:00:43 +0200

    feat: psr-4 autoloader created

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-require '../helpers.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../helpers.php';
 
 require basePath('Framework/Router.php');
 require basePath('Framework/Database.php');
