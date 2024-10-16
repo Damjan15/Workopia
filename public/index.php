a4f49344c366726135b74ef7b5c83c47dac8db64
Damjan15 <stamenovdamjan5@gmail.com>
2024-10-16 14:08:20 +0200

    feat: namespace for Framework created

--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
 <?php
 
+use Framework\Router;
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../helpers.php';
 
 require basePath('Framework/Router.php');
 require basePath('Framework/Database.php');
 
-$router = new Router;
+$router = new Router();
 
 $routes = require basePath('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
